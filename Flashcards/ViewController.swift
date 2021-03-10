eed6aeef386a3b1fae898cb0bcf4758cb045507b
Bhuvni Shah <bhuvni.shah@gmail.com>
2021-03-10 17:03:40 -0600

    rounded the edges

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,16 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // since the card is clipped, the shaddows will not show up
+        card.layer.cornerRadius = 20.0
+//        card.clipsToBounds = true
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        frontLabel.layer .cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        
+        backLabel.layer.cornerRadius = 20.0
+        backLabel.clipsToBounds = true
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true;
+        if(frontLabel.isHidden){
+            frontLabel.isHidden = false;
+        }
+        else{
+            frontLabel.isHidden = true;
+        }
     }
     
 }
